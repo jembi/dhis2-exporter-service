a1669c77ccc096e7b55f2cdc72cb91cf655e99d5
tresor <tresorluz21@gmail.com>
2023-06-24 20:46:30 +0200

    ...

--- src/main/java/org/jembi/ciol/AppConfig.java
+++ src/main/java/org/jembi/ciol/AppConfig.java
@@ -22,7 +22,7 @@ public final class AppConfig {
             .withResource("application.conf")
             .build();
 
-    public static final String KAFKA_BOOTSTRAP_SERVERS = CONFIG.getString("KAFKA_HOSTS");
+    public static final String KAFKA_BOOTSTRAP_SERVERS = CONFIG.getString("kafka.bootstrap.servers");
     public static final String KAFKA_APPLICATION_ID = CONFIG.getString("kafka.application-id");
     public static final String KAFKA_CLIENT_ID = CONFIG.getString("kafka.client-id");
     public static final String HTTP_SERVER_HOST = CONFIG.getString("http-server.host");
